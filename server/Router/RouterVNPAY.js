eb7b47fd87fde09da8ccf718606a3fb974449db9
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-11-08 18:36:37 +0700

    update

--- server/Router/RouterVNPAY.js
+++ server/Router/RouterVNPAY.js
@@ -74,7 +74,8 @@ router.post('/create_payment_url', function (req, res, next) {
     let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
     vnp_Params['vnp_SecureHash'] = signed;
     vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
-    res.redirect(vnpUrl)
+
+    res.status(200).json({ data: vnpUrl })
 });
 router.get('/vnpay_return', function (req, res, next) {
     let vnp_Params = req.query;
